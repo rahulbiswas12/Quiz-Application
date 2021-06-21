70869ddea438390998a772d5333862f3c2252c0f
Jaydeep Das <49182159+jaydeepdas@users.noreply.github.com>
2021-06-21 09:27:07 +0530

    Update opt1.java

--- src/simple/minds/opt1.java
+++ src/simple/minds/opt1.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor...
  */
 package simple.minds;
 
@@ -10,5 +10,6 @@ package simple.minds;
  * @author Rahul
  */
 class opt1 {
+ 
     
 }
